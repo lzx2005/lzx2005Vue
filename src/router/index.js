7d6c5bb14a6d53a6b160175a1a1c193b7b3a0124
李正先 <hzlizx@tairanchina.com>
2018-02-01 16:37:41 +0800

    基础设置，hello world

--- src/router/index.js
+++ src/router/index.js
@@ -1,21 +1,9 @@
-import Vue from 'vue'
-import Router from 'vue-router'
-import HelloWorld from '@/components/HelloWorld'
-import Index from '@/components/Index'
+import Vue from 'vue';
+import Router from 'vue-router';
 
-Vue.use(Router)
+Vue.use(Router);
 
 export default new Router({
   routes: [
-    {
-      path: '/',
-      name: 'HelloWorld',
-      component: HelloWorld
-    },
-    {
-      path: '/index',
-      name: 'Index',
-      component: Index
-    }
   ]
-})
+});
