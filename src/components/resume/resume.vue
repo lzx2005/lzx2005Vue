41adddd75cedd117f1b7c6100eea91655a5a045d
李正先 <hzlizx@tairanchina.com>
2018-02-09 11:57:53 +0800

    修改简历模块

--- src/components/resume/resume.vue
+++ src/components/resume/resume.vue
@@ -1,8 +1,39 @@
 <template>
-    <Card class="card">
-      <p slot="title">No border title</p>
-      <p>Content of no border type. Content of no border type. Content of no border type. Content of no border type. </p>
-    </Card>
+  <!--<Card class="card">-->
+  <!--<p slot="title">个人简历</p>-->
+  <!--<p>这个地方是放个人简历的地方，虽然我也不知道现在放什么好，但是还是打算先看一下。</p>-->
+  <!--</Card>-->
+
+  <Timeline>
+    <TimelineItem>
+      <p class="time">2014年9月</p>
+      <p class="content">获得服务外包大赛全国二等奖，三次重构</p>
+      <img class="content-img" src="./img/img3.jpeg"/>
+    </TimelineItem>
+    <TimelineItem>
+      <p class="time">2014年5月</p>
+      <p class="content">获得电子商务大赛浙江省一等奖</p>
+      <img class="content-img" src="./img/img2.jpeg"/>
+    </TimelineItem>
+    <TimelineItem>
+      <p class="time">2014年3月</p>
+      <p class="content">
+        开始参与学校竞赛，主要在组内担任Java和前端的研发，获得电子商务大赛杭电站一等奖。
+        <br/>
+        项目名称为沿途，一个基于LBS的社会化电子导游信息，本人负责后台和前端的开发，同伴负责APP的研发工作。
+      </p>
+      <img class="content-img" src="./img/img1.jpeg"/>
+    </TimelineItem>
+    <TimelineItem>
+      <p class="time">2014年1月</p>
+      <p class="content">由计算机专业转入电子信息工程专业</p>
+    </TimelineItem>
+    <TimelineItem>
+      <p class="time">2013年9月</p>
+      <p class="content">进入杭州电子科技大学信息工程学院计算机系</p>
+    </TimelineItem>
+  </Timeline>
+
 </template>
 
 <script type="text/ecmascript-6">
@@ -12,4 +43,16 @@
 <style lang="stylus" rel="stylesheet/stylus">
   .card
     margin 0px auto
+
+  .time
+    font-size 14px
+    font-weight bold
+
+  .content
+    padding-left 5px
+  .content-img
+    padding 5px
+    padding-left 5px
+    border 1px solid #f5f5f5
+    width 60%
 </style>
